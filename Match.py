12724e54b1c97a5075f36a48f6ca7ccca2e93824
Tai Vo Tuan <vtuantai@tma.com.vn>
2022-06-07 14:16:57 +0700

    Done compare

--- Match.py
+++ Match.py
@@ -1,3 +1,4 @@
+from aifc import Error
 from Card import Card
 from Customer import Customer
 from House import House
@@ -31,55 +32,63 @@ class Match:
         cardShow1 = card1
         cardShow2 = card2
         
-        match f"{card1.group}":
-            case 'A':
-                card1 = Card(int("1"), card1.suites, card1.color)
-            case 'J':
-                card1 = Card(int("11"), card1.suites, card1.color)
-            case 'Q': 
-                card1 = Card(int("12"), card1.suites, card1.color)
-            case 'K':
-                card1 = Card(int("13"), card1.suites, card1.color)
-            case 'Joker':
-                card1 = Card(int("14"), card1.suites, card1.color)
-        match f"{card2.group}":
-            case 'A':
-                card2 = Card(int("1"), card1.suites, card1.color)
-            case 'J':
-                card2 = Card(int("11"), card1.suites, card1.color)
-            case 'Q': 
-                card2 = Card(int("12"), card1.suites, card1.color)
-            case 'K':
-                card2 = Card(int("13"), card1.suites, card1.color)
-            case 'Joker':
-                card2 = Card(int("14"), card1.suites, card1.color)
+        try:
+            match f"{card1.group}":
+                case 'A':
+                    card1 = Card(int("1"), card1.suites, card1.color)
+                case 'J':
+                    card1 = Card(int("11"), card1.suites, card1.color)
+                case 'Q': 
+                    card1 = Card(int("12"), card1.suites, card1.color)
+                case 'K':
+                    card1 = Card(int("13"), card1.suites, card1.color)
+                case 'Joker':
+                    card1 = Card(int("14"), card1.suites, card1.color)
+            match f"{card2.group}":
+                case 'A':
+                    card2 = Card(int("1"), card2.suites, card2.color)
+                case 'J':
+                    card2 = Card(int("11"), card2.suites, card2.color)
+                case 'Q': 
+                    card2 = Card(int("12"), card2.suites, card2.color)
+                case 'K':
+                    card2 = Card(int("13"), card2.suites, card2.color)
+                case 'Joker':
+                    card2 = Card(int("14"), card2.suites, card2.color)
+            
+            match f"{card1.suites}":
+                case 'Heart':
+                    card1 = Card(card1.group, int("24"), card1.color)
+                case 'Diamond':
+                    card1 = Card(card1.group, int("23"), card1.color)
+                case 'Club':
+                    card1 = Card(card1.group, int("22"), card1.color)
+                case 'Spade':
+                    card1 = Card(card1.group, int("21"), card1.color)
+                case '':
+                    card1 = Card(card1.group, int("0"), card1.color)
+            match f"{card2.suites}":
+                case 'Heart':
+                    card2 = Card(card2.group, int("24"), card2.color)
+                case 'Diamond':
+                    card2 = Card(card2.group, int("23"), card2.color)
+                case 'Club':
+                    card2 = Card(card2.group, int("22"), card2.color)
+                case 'Spade':
+                    card2 = Card(card2.group, int("21"), card2.color)
+                case '':
+                    card2 = Card(card2.group, int("0"), card2.color)
+        except Error:
+            print("Can't Convert")
         
-        match f"{card1.suites}":
-            case 'Heart':
-                card1 = Card(card1.group, int("24"), card1.color)
-            case 'Diamond':
-                card1 = Card(card1.group, int("23"), card1.color)
-            case 'Club':
-                card1 = Card(card1.group, int("22"), card1.color)
-            case 'Spade':
-                card1 = Card(card1.group, int("21"), card1.color)
-        match f"{card2.suites}":
-            case 'Heart':
-                card2 = Card(card2.group, int("24"), card1.color)
-            case 'Diamond':
-                card2 = Card(card2.group, int("23"), card1.color)
-            case 'Club':
-                card2 = Card(card2.group, int("22"), card1.color)
-            case 'Spade':
-                card2 = Card(card2.group, int("21"), card1.color)
-                
-        ##print(f"{int(card1.group)} {int(card2.group)}")
         if int(card1.group) > int(card2.group):
-            print(f"{cardShow1.group} > {cardShow2.group}")
+            print(f"{cardShow1.group} {cardShow1.suites} {cardShow1.color} > {cardShow2.group} {cardShow2.suites} {cardShow2.color}")
         elif (int(card1.group) == int(card2.group)) and (int(card1.suites) > int(card2.suites)):
-            print(f"{cardShow1.suites} > {cardShow2.suites}")
-        else: return False
-        
-        
+            print(f"{cardShow1.group} {cardShow1.suites} {cardShow1.color} > {cardShow2.group} {cardShow2.suites} {cardShow2.color}")
+        elif  int(card1.group) < int(card2.group):
+            print (f"{cardShow1.group} {cardShow1.suites} {cardShow1.color} < {cardShow2.group} {cardShow2.suites} {cardShow2.color}")
+        elif int(card1.suites) == 0 and f"{card1.color}" is 'Red' and int(card2.suites) == 0 and f"{card2.color}" is 'Black':
+            print(f"{cardShow1.group} {cardShow1.suites} {cardShow1.color} > {cardShow2.group} {cardShow2.suites} {cardShow2.color}")
+        else: print(f"{cardShow1.group} {cardShow1.suites} {cardShow1.color} < {cardShow2.group} {cardShow2.suites} {cardShow2.color}")
 
         
